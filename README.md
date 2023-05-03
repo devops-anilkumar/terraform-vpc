acc0cdf29a7f24eaca938f1ba7ee4a669e592880
unknown <anilkumarravuri510@gmail.com>
2023-05-03 20:47:08 +0530

    auto push

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # terraform-vpc
+
+this is the vpc root module, all the vpc related information  is going to be placed here
+actual vpc terraform code is placed in backend module.
---
+++ main.tf
@@ -0,0 +1,5 @@
+module "vpc" {
+  source = "https://github.com/devops-anilkumar/tf-module-vpc.git"
+}
+
+#THIS ALWAYS GOES WITH THE MAIN BRANCH , TELL THE CODE TO PICK FROM INTENDED BRANCH
