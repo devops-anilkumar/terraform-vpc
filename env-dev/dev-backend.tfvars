df9a833b115e223cb3c92040fbbe773bbf19c73b
unknown <anilkumarravuri510@gmail.com>
2023-05-03 21:26:38 +0530

    auto push

---
+++ env-dev/dev-backend.tfvars
@@ -0,0 +1,3 @@
+    bucket = "state-terraformbucket"
+    key    = "vpc/dev/terraform.tfstate"
+    region = "us-east-1"
--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 module "vpc" {
-  source = "https://github.com/devops-anilkumar/tf-module-vpc.git"
+  source = "vendor/modules/vpc"
+  VPC_CIDR = var.VPC_CIDR
 }
 
 #THIS ALWAYS GOES WITH THE MAIN BRANCH , TELL THE CODE TO PICK FROM INTENDED BRANCH
