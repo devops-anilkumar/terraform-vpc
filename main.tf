a912a912c0ec71ef2ae54d3c94db7bb912766429
unknown <anilkumarravuri510@gmail.com>
2023-05-04 01:41:35 +0530

    auto push

--- main.tf
+++ main.tf
@@ -1,7 +1,10 @@
 module "vpc" {
-  source    = "./vendor/modules/vpc"
-  VPC_CIDR  = var.VPC_CIDR
-  ENV       = var.ENV
+  source               = "./vendor/modules/vpc"
+  VPC_CIDR             = var.VPC_CIDR
+  ENV                  = var.ENV
+  PUBLIC_SUBNET_CIDR   = var.PUBLIC_SUBNET_CIDR
+  PRIVATE_SUBNET_CIDR  = var.PRIVATE_SUBNET_CIDR
+  AZ                   = var.AZ
 }
 
 #THIS ALWAYS GOES WITH THE MAIN BRANCH , TELL THE CODE TO PICK FROM INTENDED BRANCH
