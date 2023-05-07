65425d6a1659d830031521a7647a70758d9dbe21
unknown <anilkumarravuri510@gmail.com>
2023-05-07 16:55:05 +0530

    auto push

--- main.tf
+++ main.tf
@@ -1,14 +1,17 @@
 module "vpc" {
-  source                 = "./vendor/modules/vpc"
-  VPC_CIDR               = var.VPC_CIDR
-  ENV                    = var.ENV
-  PUBLIC_SUBNET_CIDR     = var.PUBLIC_SUBNET_CIDR
-  PRIVATE_SUBNET_CIDR    = var.PRIVATE_SUBNET_CIDR
-  AZ                     = var.AZ
-  DEFAULT_VPC_ID         = var.DEFAULT_VPC_ID
-  DEFAULT_VPC_CIDR       = var.DEFAULT_VPC_CIDR
-  DEFAULT_VPC_RT         = var.DEFAULT_VPC_RT
-  PRIVATE_HOSTED_ZONE_ID = var.PRIVATE_HOSTED_ZONE_ID
-}
+  source                    = "./vendor/modules/vpc"
+  VPC_CIDR                  = var.VPC_CIDR
+  ENV                       = var.ENV
+  PUBLIC_SUBNET_CIDR        = var.PUBLIC_SUBNET_CIDR
+  PRIVATE_SUBNET_CIDR       = var.PRIVATE_SUBNET_CIDR
+  AZ                        = var.AZ
+  DEFAULT_VPC_ID            = var.DEFAULT_VPC_ID
+  DEFAULT_VPC_CIDR          = var.DEFAULT_VPC_CIDR
+  DEFAULT_VPC_RT            = var.DEFAULT_VPC_RT
+  PRIVATE_HOSTED_ZONE_ID    = var.PRIVATE_HOSTED_ZONE_ID
+  PRIVATE_HOSTED_ZONE_NAME  = var.PRIVATE_HOSTED_ZONE_NAME
+#   PUBLIC_HOSTED_ZONE_ID     = var.PUBLIC_HOSTED_ZONE_ID
+#   PUBLIC_HOSTED_ZONE_NAME   = var.PUBLIC_HOSTED_ZONE_NAME
+ }
 
 #THIS ALWAYS GOES WITH THE MAIN BRANCH , TELL THE CODE TO PICK FROM INTENDED BRANCH
